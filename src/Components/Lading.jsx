6c01145c4be7862ee5abed79c264d13733f16310
smitapawar0195 <smitapawar0195@gmail.com>
2023-05-22 18:16:16 +0530

    Added Home Page

--- src/Components/Lading.jsx
+++ src/Components/Lading.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-
+import React from "react";
+import { Home } from "./Home";
 const Lading = () => {
-    return (
-        <div>
-            <h2>Lading Component</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Lading Component</h2>
+      <h2>
+        <Home />
+      </h2>
+    </div>
+  );
 };
 
 export default Lading;
